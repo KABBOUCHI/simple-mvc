64ed80bd8e8d5645732d7edc851198527afa1dc0
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2017-12-16 19:28:24 +0200

    refactor redirect function

--- src/helpers.php
+++ src/helpers.php
@@ -111,9 +111,12 @@ if (!function_exists('session')) {
 
 if (!function_exists('redirect')) {
 
-    function redirect($url)
+    function redirect($url, $permanent = false)
     {
-        header('Location: ' . $url);
+        if (headers_sent() === false) {
+            header('Location: ' . $url, true, $permanent ? 301 : 302);
+        }
+        exit();
     }
 }
 
